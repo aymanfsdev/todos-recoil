4bf44a096c8cae29166857bea3786168fc63c249
stgitdev <stanleygeorge1112@gmail.com>
2022-12-28 13:42:02 -0500

    complete app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div className="App tc">
+        <h1 className="">
+          Todos
+        </h1>
+        <div className="list pt3 br3">
+          <Todos />
+          <AddTodo />
+        </div>
+      </div>
+    </RecoilRoot>
+  );
+}
+
+export default App;
